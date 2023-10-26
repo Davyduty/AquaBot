9bd7001b4df4bed9ab701e4a49417e66bebe1d57
Davyduty <129392472+Davyduty@users.noreply.github.com>
2023-10-26 08:40:14 +0000

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,11 +1,11 @@
 function successfullMessage(msg) {
-    return "✅ *AQUA*:  ```" + msg + "```"
+    return "✅ *Davyduty*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *AQUA*:  ```" + msg + "```"
+    return "🛑 *Davyduty*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *AQUA*:  ```" + msg + "```"
+    return "⏺️ *Davyduty*:  ```" + msg + "```"
 }
 
 
